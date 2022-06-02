24ac7048a64836ed46e4d66c6d92ba6fedefee49
Zapadlo <zapadlo_13@mail.ru>
2022-06-02 10:00:52 +0300

    push lesson_2_part_2

--- Messenger/unit_tests/test_client.py
+++ Messenger/unit_tests/test_client.py
@@ -1,35 +1,30 @@
-"""Unit-тесты клиента"""
-
 import sys
-import os
+sys.path.append('../')
+from client import create_presence, process_response_ans
+from common.variables import *
 import unittest
-sys.path.append(os.path.join(os.getcwd(), '..'))
-from common.variables import RESPONSE, ERROR, USER, ACCOUNT_NAME, TIME, ACTION, PRESENCE
-from client import create_presence, process_ans
+from errors import ReqFieldMissingError, ServerError
 
+
+# Класс с тестами
 class TestClass(unittest.TestCase):
-    '''
-    Класс с тестами
-    '''
-
+    # тест коректного запроса
     def test_def_presense(self):
-        """Тест коректного запроса"""
-        test = create_presence()
-        test[TIME] = 1.1  # время необходимо приравнять принудительно
-                          # иначе тест никогда не будет пройден
+        test = create_presence('Guest')
+        test[TIME] = 1.1  # время необходимо приравнять принудительно иначе тест никогда не будет пройден
         self.assertEqual(test, {ACTION: PRESENCE, TIME: 1.1, USER: {ACCOUNT_NAME: 'Guest'}})
 
+    # тест корректтного разбора ответа 200
     def test_200_ans(self):
-        """Тест корректтного разбора ответа 200"""
-        self.assertEqual(process_ans({RESPONSE: 200}), '200 : OK')
+        self.assertEqual(process_response_ans({RESPONSE: 200}), '200 : OK')
 
+    # тест корректного разбора 400
     def test_400_ans(self):
-        """Тест корректного разбора 400"""
-        self.assertEqual(process_ans({RESPONSE: 400, ERROR: 'Bad Request'}), '400 : Bad Request')
+        self.assertRaises(ServerError, process_response_ans , {RESPONSE: 400, ERROR: 'Bad Request'})
 
+    # тест исключения без поля RESPONSE
     def test_no_response(self):
-        """Тест исключения без поля RESPONSE"""
-        self.assertRaises(ValueError, process_ans, {ERROR: 'Bad Request'})
+        self.assertRaises(ReqFieldMissingError, process_response_ans, {ERROR: 'Bad Request'})
 
 
 if __name__ == '__main__':
